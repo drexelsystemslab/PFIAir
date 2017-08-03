a5c24d8a107bcb30c2e54fa02db60e64fa0b55e6
mudkipmaster <acg59@drexel.edu>
2017-08-03 14:46:19 -0400

    fixed search fend bug

--- PFI_Python/main.py
+++ PFI_Python/main.py
@@ -10,9 +10,7 @@ import numpy as np
 
 from mpl_toolkits import mplot3d
 
-
-name = 'chair'
-name = '20000_polygon_sphere_100mm'
+name = 'gradienttest'
 
 url = 'models/'+name+'.stl'
 fileName = url.split('/')[-1]
@@ -23,13 +21,7 @@ except(OSError,IOError,ValueError):
     print("31: stl file missing")
     raise IOError
 
-#ToolBox.faceDetector(model)
-#ToolBox.localNeighborhoods(model)
-facets = ToolBox.randomWalker(model)
-for facet in facets:
-    model.visual.face_colors[facet] = [252, 154, 7, 255]
-
-model.show()
+Demos.svd(model)
 
 
 
