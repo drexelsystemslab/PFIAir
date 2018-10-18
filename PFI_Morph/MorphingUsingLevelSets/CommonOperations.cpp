64bb45920423d37e0f5ac39a3d5a55e62ab89c66
Peter Gagliardi <ptrgags@gmail.com>
2018-10-18 07:28:44 -0400

    Document CommonOperations.{h,cpp}
    I am in the process of documenting old code to help me learn how the
    existing codebase works.

--- PFI_Morph/MorphingUsingLevelSets/CommonOperations.cpp
+++ PFI_Morph/MorphingUsingLevelSets/CommonOperations.cpp
@@ -2,12 +2,16 @@
 
 namespace CommonOperations {
 
-    openvdb::FloatGrid::Ptr getPlatonicVolume(int num_faces, float scale, float voxel_size, float half_width) {
-        return openvdb::tools::createLevelSetPlatonic<openvdb::FloatGrid>(num_faces, scale, openvdb::Vec3f(0.0f), voxel_size, half_width);
+    openvdb::FloatGrid::Ptr getPlatonicVolume(
+            int num_faces, float scale, float voxel_size, float half_width) {
+        return openvdb::tools::createLevelSetPlatonic<openvdb::FloatGrid>(
+            num_faces, scale, openvdb::Vec3f(0.0f), voxel_size, half_width);
     }
 
-    openvdb::FloatGrid::Ptr getSphereVolume(float radius, float voxel_size, float half_width) {
-        return openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(radius, openvdb::Vec3f(0.0f), voxel_size, half_width);
+    openvdb::FloatGrid::Ptr getSphereVolume(
+            float radius, float voxel_size, float half_width) {
+        return openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(
+            radius, openvdb::Vec3f(0.0f), voxel_size, half_width);
     }
 
     void makeDirs(const char *dir) {
@@ -15,16 +19,24 @@ namespace CommonOperations {
         char *p = NULL;
         size_t len;
         
+        // Copy the directory name to a buffer
         snprintf(tmp, sizeof(tmp),"%s",dir);
         len = strlen(tmp);
+
+        // Remove trailing slash if it exists
         if(tmp[len - 1] == '/')
             tmp[len - 1] = 0;
+
+        // Iterate over the directory path
         for(p = tmp + 1; *p; p++)
+            // Every time we reach a slash, make the directory
             if(*p == '/') {
                 *p = 0;
                 mkdir(tmp, S_IRWXU);
                 *p = '/';
             }
+
+        // Make the final directory in the path
         mkdir(tmp, S_IRWXU);
     } 
 
@@ -35,34 +47,51 @@ namespace CommonOperations {
     }
     
     std::string getFileNameWithoutExtension(std::string file_name, std::string ext) {
-        if(ext == "") ext = ".";
+        // Make sure we have an extension
+        if(ext == "") 
+            ext = ".";
+
+        // Grab everything up to the filename
         std::size_t index = file_name.find(ext);
         file_name = file_name.substr(0, index);
         return file_name;
     }
 
-    double calc3dDistance(openvdb::Vec3d pt1, openvdb::Vec3d pt2) {
-        
+    double calc3dDistance(openvdb::Vec3d pt1, openvdb::Vec3d pt2) { 
+        // TODO: Try using vector operations
+        // length(pt1 - p2) is simpler to write and avoids Pow()
+
+        // r^2 = (x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2
         double sq_sum = openvdb::math::Pow(pt1.x() - pt2.x(), 2) +
                         openvdb::math::Pow(pt1.y() - pt2.y(), 2) +
                         openvdb::math::Pow(pt1.z() - pt2.z(), 2);
         
+        // return r = sqrt(r^2)
         return openvdb::math::Sqrt(sq_sum);
     }
 
     std::string intNumberFormatCommas(std::string format_str) {
-        std::vector<std::string> str_arr = CommonOperations::prep_do_split(format_str, '.');
+        // TODO: Something tells me there is an easier way to do this
+        // perhaps in <iomanip> or Boost
+
+        // Split on the decimal point
+        std::vector<std::string> str_arr = CommonOperations::prep_do_split(
+            format_str, '.');
+        // integer part
         std::string str = str_arr[0];
         
+        // Fractional part will go here
         std::string decimal = "";
         
         int str_length = (int)str.length() - 1,
         arr_length = str_length + (int)(str_length / 3);
         
         if(str_length < 3) {
+            // If we have an integer, we are done
             if(str_arr.size() != 2) return str;
             else {
                 decimal = str_arr[1];
+                // Only include decimals for small number
                 if(str.length() == 1) {
                     str += "." + decimal.substr(0, 2);
                 }
@@ -75,6 +104,7 @@ namespace CommonOperations {
         
         char char_arr[arr_length];
         
+        // Make a new string with commas
         for(int i = 0, j = 0; i <= str_length; i++, j++) {
             char_arr[j] = str[i];
             if((str_length - i) % 3 == 0 && i != str_length) {
@@ -82,6 +112,7 @@ namespace CommonOperations {
             }
         }
         
+        // Return it as a string
         std::string return_str(char_arr);
         return return_str.substr(0, arr_length + 1);
     }
--- PFI_Morph/MorphingUsingLevelSets/CommonOperations.h
+++ PFI_Morph/MorphingUsingLevelSets/CommonOperations.h
@@ -7,22 +7,82 @@
 #include <openvdb/tools/LevelSetPlatonic.h>
 #include <sys/stat.h>   //mkdir
 
+/**
+ * This namespace holds a number of utility functions.
+ */
 namespace CommonOperations {
     
+    /**
+     * Log a message to stdout
+     * NOTE: This is only used in Tests.h
+     * The format is
+     * <tag> - <msg>
+     * or
+     * <msg> if tag is ""
+     */
     template<class T1, class T2>
     void display(const T1 tag, const T2 msg) {
-        tag != "" ? std::cout << tag << " - " << msg << std::endl : std::cout << msg << std::endl;
+        if (tag != "")
+            std::cout << tag << " - " << msg << std::endl;
+        else
+            std::cout << msg << std::endl;
     }
+
     
+    /**
+     * Create a level set of a platonic solid, centered at the origin.
+     * This is a shortcut for the OpenVDB version without the center 
+     * parameter.
+     * 
+     * num_faces - number of sides on the platonic solid
+     * scale - how much to scale up the shape in world units
+     * voxel_size - size of one voxel in world units
+     * half_width - half the width of the narrow band around the
+     *  surface
+     */
     openvdb::FloatGrid::Ptr getPlatonicVolume(
         int num_faces, float scale, float voxel_size, float half_width);
 
+    /**
+     * Create a level set of a sphere, centered at the origin.
+     * This is a shortcut for the OpenVDB version without the center parameter
+     *
+     * radius - radius of the sphere
+     * voxel_size - size of one voxel in world units
+     * half_width - half the width of the narrow band around the
+     *  surface
+     */
     openvdb::FloatGrid::Ptr getSphereVolume(
         float radius, float voxel_size, float half_width);
     
-    void makeDirs(const char *dir);
+    /**
+     * Make a directory, including any intermediate directories in the path.
+     * e.g. makeDirs("foo/bar/baz/") creates
+     * foo/
+     * foo/bar/
+     * foo/bar/baz/
+     * 
+     * This is much like the Linux command `mkdir -p`
+     */
+    void makeDirs(const char *dir); 
     
-    
+    /**
+     * Split a string and return the parts as a vector. This calls
+     * split(), defined below.
+     */
+    std::vector<std::string> prep_do_split(const std::string &s, char delim);
+
+    /**
+     * Split a string on a delimiter
+     * and store the results in an output collection
+     * 
+     * Note: This is the low-level version. prep_do_split() is the
+     * high-level version
+     *
+     * Note: result must be an output operator supporting
+     * ++ and *. std::back_inserter() is one such iterator used
+     * elsewhere in this code.
+     */
     template<typename Out>
     void split(const std::string &s, char delim, Out result) {
         std::stringstream ss;
@@ -32,14 +92,24 @@ namespace CommonOperations {
             *(result++) = item;
         }
     }
-    
-    std::vector<std::string> prep_do_split(const std::string &s, char delim);
 
+    /**
+     * Strip off the extension of a filename.
+     */
     std::string getFileNameWithoutExtension(
         std::string file_name, std::string ext);
     
+    /**
+     * Calculate Euclidean (L2) distance between pt1 and pt2
+     */
     double calc3dDistance(openvdb::Vec3d pt1, openvdb::Vec3d pt2);
     
+    /**
+     * Format a number with commas for the thousands separator
+     * the input is an int already cast to a string.
+     *
+     * TODO: Why not pass in an int and do the casting in the method?
+     */
     std::string intNumberFormatCommas(std::string format_str);
 }
 
